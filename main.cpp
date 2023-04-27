77167c015fb0b0bbb2637a8b872855a6c4717742
ArturNunes21 <102315342+ArturNunes21@users.noreply.github.com>
2023-04-26 22:03:39 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,36 +1,128 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    ifstream arqRead;
-    ofstream alunos;
-    string line="";
-    string number="";
-    string name="";
+    std::ifstream arqRead;
+    std::ofstream alunos, disciplinas, matriculas;
+    vector<string>vector{""};
+    std::string line, matricula, name, codigoDisciplina, materia, turma;
+    line=matricula=name=codigoDisciplina=materia=turma="";
+    bool encontrado=false;
 
     arqRead.open("CMP-2.txt");
     if(arqRead.is_open()){
         alunos.open("alunos.txt");
-        while(getline(arqRead, line)){
-            if(!line.empty()){
+        while(std::getline(arqRead, line)){
+            if(line.size()>4 && line[8]=='.' && !line.empty()){
                 for(int i=3; i<18;i++){
-                    number+=line[i];
+                    matricula+=line[i];
                 }
-
-                for(int i=25; i<line.size();i++){
-                    name+=line[i];
+                for(int i=25; i<line.size(); i++){
+                    if (i + 2 < line.size() && line[i] == ' ' && line[i+1] == ' ') {
+                        // Se os próximos dois índices forem vazios, pula para a próxima linha
+                        break;
+                    }
+                    name += line[i];
                 }
-                alunos<<number<<" "<<name<<endl;
-                name="";
-                number="";
             }
-        }
+                encontrado=false;
+                for(int i=0;i<vector.size();i++){
+                    if(vector[i]==matricula){
+                        encontrado=true;
+                        break;
+                    }
+                }
+                if(matricula!="" && name!="" && !encontrado){
+                    vector.push_back(matricula);  //adiciona matricula no vetor
+                    alunos<<matricula<<" "<<name<<std::endl;   //Adiciona nome e matricula no arquivo
+                }
+            name="";
+            matricula="";
+            }
         arqRead.close();
         alunos.close();
-        cout<<"Concluido";
+        vector.clear();
+        std::cout<<"Arquivo 'alunos' criado com sucesso!"<<std::endl;   
     }else{
-        cout<<"Nao foi possivel abrir o arquivo."<<endl;
+        std::cout<<"Nao foi possivel abrir o arquivo."<<std::endl;
+        return 0;
+    }
+
+
+    arqRead.open("CMP-2.txt");
+    if(arqRead.is_open()){
+        disciplinas.open("disciplinas.txt");
+        while(std::getline(arqRead, line)){
+            if(line[78]=='-'){
+             for(int i=70; line[i]!=' '; i++){
+                codigoDisciplina+=line[i];
+             }
+             for(int i=80; i<line.size(); i++){
+                if (i + 2 < line.size() && line[i] == ' ' && line[i+1] == ' ')
+                    break;
+                materia+=line[i];
+                }   
+            }
+            encontrado=false;
+            for(int i=0;i<vector.size();i++){
+                if(vector[i]==codigoDisciplina){
+                    encontrado=true;
+                    break;
+                }
+            }
+            if(codigoDisciplina!="" && materia!="" && !encontrado){
+                vector.push_back(codigoDisciplina); //codigo é adicionado ao vector
+                disciplinas<<codigoDisciplina<<" "<<materia<<std::endl; //codigo e nome da materia são impressos no arquivo
+            }
+            codigoDisciplina="";
+            materia="";
+        }
+        arqRead.close();
+        disciplinas.close();
+        vector.clear();
+        std::cout<<"Arquivo 'disciplinas' criado com sucesso!"<<std::endl;
+    }else{
+        std::cout<<"Nao foi possivel abrir o arquivo."<<std::endl;
+        return 0;
+    }
+    
+    arqRead.open("CMP-2.txt");
+    if(arqRead.is_open()){
+        int espaco=-1;
+        matriculas.open("matriculas.txt");
+        while(std::getline(arqRead, line)){
+            espaco++;
+            if(line[78]=='-'){
+             for(int i=70; i<77; i++){
+                codigoDisciplina+=line[i];
+             }
+             for(int i=135; i<line.size(); i++){
+                turma+=line[i];
+                }
+            }
+            if(line.size()>4 && line[8]=='.'){
+            for(int i=3; i<18;i++){
+                    matricula+=line[i];
+                }
+            }   
+            if(codigoDisciplina!="" && turma!="" &&espaco>1){
+                matriculas<<"       *       "<<std::endl;
+                matriculas<<"=="<<codigoDisciplina<<" "<<turma<<"=="<<std::endl;
+            }
+            if(matricula!="")
+                matriculas<<matricula<<std::endl;
+            codigoDisciplina="";
+            turma="";
+            matricula="";
+            }
+        arqRead.close();
+        matriculas.close();
+        std::cout<<"Arquivo 'matriculas' criado com sucesso!"<<std::endl;
+    }else{
+        std::cout<<"Nao foi possivel abrir o arquivo."<<std::endl;
+        return 0;
     }
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,28 @@
-o
+arqRead.open("CMP-2.txt");
+    if(arqRead.is_open()){
+        alunos.open("alunos.txt");
+        while(getline(arqRead, line)){
+            if(line.size()>4 && line[8]=='.'){
+                for(int i=3; i<18;i++){
+                    number+=line[i];
+                }
+                for(int i=135; i<line.size(); i++){
+                    if (i + 2 < line.size() && line[i+1] == ' ' && line[i+2] == ' ') {
+                        // Se os próximos dois índices forem vazios, pula para a próxima linha
+                        break;
+                }
+                    name += line[i];
+                }
+                }
+                if(number!="" && name!="")
+                    alunos<<number<<" "<<name<<endl;
+                name="";
+                number="";
+            }
+        arqRead.close();
+        alunos.close();
+        cout<<"Arquivo 'alunos' criado com sucesso!"<<endl;   
+    }else{
+        cout<<"Nao foi possivel abrir o arquivo."<<endl;
+        return 0;
+    }
